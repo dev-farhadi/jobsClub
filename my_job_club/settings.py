5e5c53f3a58a6634cb569f85185778f2434ae73e
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-20 14:23:04 +0330

    connect to db and create urls

--- my_job_club/settings.py
+++ my_job_club/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'my_job_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jobdb',
+        'USER': 'root',
+       # 'PASSWORD': ' ',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
