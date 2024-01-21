f03838f71f7f67a823da6129856de1fdb22d5e4e
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-21 09:47:30 +0330

    add some styles into sign up page

--- JobsApp/urls.py
+++ JobsApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('signup/', views.signup, name='signup'),
-    path('success/', views.success, name='success')
+    path('', views.home, name='home')
 ]
--- JobsApp/views.py
+++ JobsApp/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from .models import User
 from django.http import HttpResponse
 from django.template import loader
-import time
 
 def signup(request):
     if request.method == 'POST':
@@ -11,17 +10,24 @@ def signup(request):
         username = request.POST['username']
         password = request.POST['password']
 
-        user = User(username=username, password=password, name=name, family=family)
-        user.save()
-        return redirect('success') # Redirect to a success page   
+
+        if User.objects.filter(username=username).exists():
+           template= loader.get_template('signup.html')
+           context = {
+              'error': 'bad bad'
+           }
+           return HttpResponse(template.render(context, request))
+        else:
+         user = User(username=username, password=password, name=name, family=family)
+         user.save()
+         return redirect('home') # Redirect to a success page   
 
     return render(request, 'signup.html')
 
-def success(request):
-  template = loader.get_template('success.html')
- #time.sleep(3)
-  #return redirect('signup')
+def home(request):
+  template = loader.get_template('home.html')
 
   return HttpResponse(template.render())
+   
 
 # Create your views here.
