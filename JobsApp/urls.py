236c2abece4e547db97d98eb9e2c1fa9beb3e1a2
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-23 11:55:31 +0330

    its done

--- JobsApp/urls.py
+++ JobsApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import signup, loginView, homeView, logoutView, applyView
+from .views import signup, loginView, homeView, logoutView, applyView, jobsView
 
 urlpatterns = [
 
    path('<int:jj>/', applyView, name="apply"),
+   path('jobs/', jobsView, name="jobs"),
    path('home/', homeView, name="home"),
    path('signup/', signup, name="signup"),
    path('login/', loginView, name="login"),
--- JobsApp/views.py
+++ JobsApp/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Jobs,JobsUser
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 
 def signup(request):
     if request.method == 'POST':
@@ -39,13 +40,26 @@ def logoutView(request):
    
 
 def homeView(request):
-    jobs = Jobs.objects.all()
-    return render(request, 'home.html', {'jobs' : jobs})
+    
+    jobs = Jobs.objects.order_by('-id')[:5]
+    paginator = Paginator(jobs, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'home.html', {'page_obj': page_obj})
 
 @login_required
 def applyView(request, jj):
+   d = JobsUser.objects.filter(job_id=jj, user_id=request.user.id).exists()
    job = Jobs.objects.get(id=jj)
    user = User.objects.get(id=request.user.id)
    instance = JobsUser(job_id=job, user_id=user)
    instance.save()
-   return render(request, 'apply.html', {'jj': jj})
+   return render(request, 'apply.html', {'d': d})
+
+
+def jobsView(request):
+   jobs = Jobs.objects.all()
+   paginator = Paginator(jobs, 5)
+   page_number = request.GET.get("page")
+   page_obj = paginator.get_page(page_number)
+   return render(request, 'jobs.html', {'page_obj' : page_obj})
