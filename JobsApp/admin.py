5febccad19b953e5bfd699fecf5edbf9a473045a
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-24 08:26:43 +0330

    set Employer model and connect to admin, set foreign key to job model

--- JobsApp/admin.py
+++ JobsApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Jobs, JobsUser
+from .models import Jobs, JobsUser, Employer
 # Register your models here.
 admin.site.register(Jobs)
 admin.site.register(JobsUser)
+admin.site.register(Employer)
 
 
--- JobsApp/models.py
+++ JobsApp/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.conf import settings
 
+class Employer(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    address = models.CharField(max_length=255, default='N/A')
+    co = models.CharField(max_length=255, default="N/A")
+
+    def __str__(self):
+        return str(self.co)
+    
 class Jobs(models.Model):
     id = models.BigAutoField(primary_key=True)
-    company = models.CharField(max_length=255)
+    employer = models.ForeignKey('Employer',on_delete=models.CASCADE, null=True)
     position = models.CharField(max_length=255)
     address = models.CharField(max_length=255, default='N/A')
     description = models.TextField(max_length=1080)
@@ -11,6 +19,7 @@ class Jobs(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
     
 class JobsUser(models.Model):
     job_id = models.ForeignKey(Jobs, on_delete=models.CASCADE)
